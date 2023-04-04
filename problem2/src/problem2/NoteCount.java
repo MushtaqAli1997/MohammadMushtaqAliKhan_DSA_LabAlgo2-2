c5d64bb1f4618eb4e7c0d3f4ab3a7fd1087197e4
MushtaqAli1997 <121460506+MushtaqAli1997@users.noreply.github.com>
2023-04-04 14:23:26 +0530

    Add files via upload

---
+++ problem2/src/problem2/NoteCount.java
@@ -0,0 +1,31 @@
+package problem2;
+
+public class NoteCount {
+
+	public void counting(int[] currency, int amount) {
+
+		int noteCount[] = new int[currency.length];
+		int i;
+		int sumofnotes = 0;
+		for (i = 0; i < currency.length; i++) {
+			noteCount[i] = amount / currency[i];
+			amount = amount % currency[i];
+			sumofnotes += noteCount[i];
+		}
+
+		for (i = 0; i < currency.length; i++)
+
+		{
+			if (noteCount[i] != 0)
+
+			{
+
+				System.out.println(currency[i] + ":" + noteCount[i]);
+			}
+		}
+
+		System.out.println("Total Notes " + sumofnotes);
+
+	}
+
+}
